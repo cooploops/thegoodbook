d9a07ebd3d3e1f8a8966667a7c881a13db6e2574
Michael Sanchez <futurefreq@hotmail.com>
2018-02-02 06:14:26 -0800

    carousel and img adjustments

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,17 +1,43 @@
 import React from "react";
 
+
+
+
+//This needs to take in props from the 'products' component
 const Carousel = (props) =>
     <div className="container">
         <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img className="d-block w-100" src="./img/gummies.jpeg" alt="First slide"></img>
+                    <img className="d-block" width={400} height={400} src="./img/gummies400px.jpeg" alt="First slide"></img>
+                    <strong>"The Gummy Book"</strong>
+                    <h6>Contains:</h6>
+                    Gummy Worms<br/>
+                    Jelly Beans<br/>
+                    Hard Candy<br/>
+                    <h6>Price:</h6>
+                    $15.99
+
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block w-100" src="./img/bakedGoods.jpeg" alt="Second slide"></img>
+                    <img className="d-block" width={400} height={400} src="./img/bakedGoods400px.jpeg" alt="Second slide"></img>
+                    <strong>"The Pastry Book"</strong>
+                    <h6>Contains:</h6>
+                    Cupcake<br/>
+                    Danish<br/>
+                    Cream Puff<br/>
+                    <h6>Price:</h6>
+                    $18.99
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block w-100" src="./img/ChocolateTreats.jpeg" alt="Third slide"></img>
+                    <img className="d-block" width={400} height={400} src="./img/ChocolateTreats400px.jpeg" alt="Third slide"></img>
+                    <strong>"The Chocolate Book"</strong>
+                    <h6>Contains:</h6>
+                    Candy Bar<br/>
+                    Cocoa Crispy Treat<br/>
+                    PB and Jays<br/>
+                    <h6>Price:</h6>
+                    $18.99
                 </div>
             </div>
             <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
