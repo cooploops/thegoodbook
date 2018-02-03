6ded2191785f5cf26a10c3c26d0a044d335c8ed8
Austin Wagner <austin.e.wagner@gmail.com>
2018-02-03 12:39:42 -0800

    testimonial set up

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Carousel from "../../components/Carousel";
-
+import Testimonials from "../../components/Testimonials"
 const Home = () =>
+<div>
   <Carousel />
-
+  <Testimonials />
+</div>
 export default Home;
