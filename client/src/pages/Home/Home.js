7b867ce684a02d7842ff10681497d57e99448291
TJ <cooploops@gmail.com>
2018-02-03 13:06:04 -0800

    working on carousel fix

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../../utils/API";
 import Carousel from "../../components/Carousel";
 
-const Home = () =>
-  <Carousel />
 
+class Home extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      products: null
+    }
+  }
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = () => {
+    API.getProducts()
+      .then(res =>
+        console.log(res.data)
+        // this.setState({products: res.data})
+      )
+      .catch(err => console.log(err));
+  };
+
+
+  render() {
+    return(
+      <Carousel />
+    )
+  }
+
+
+}
+  
 export default Home;
