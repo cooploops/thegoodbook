3b977c1943d12dd4c663a55a33380326c6181f39
TJ <cooploops@gmail.com>
2018-02-04 21:21:40 -0800

    add footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => 
+    <div className='footer'>
+        <div className='container'>
+            <h6 className="text-center pt-3">© 2018 The Good Book</h6>
+        </div>
+    </div>
+
+export default Footer;
