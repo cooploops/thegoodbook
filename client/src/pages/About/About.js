95a1057a8845acc2ed8f5b1f5baa6f42a6d6c038
Austin Wagner <austin.e.wagner@gmail.com>
2018-02-07 20:53:49 -0800

    About styling

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./About.css"
 
 const About = () =>
   <div className="container">
-    <div className="jumbotron">
-      <h1 className="display-4">Hello, world!</h1>
-      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <div className="jumbotron weed">
+      <img class="img-fluid weed-img" alt="Responsive image"></img>
       <hr className="my-4"></hr>
-        <p>Subscription boxes are as popular as ever, but one thing has been missing from the scene… Introducing Green Books, a cannabis subscription service that brings your high right to you. You avoid the hassle of having to even leave your apartment (because let’s be real, you’re still living in an apartment). Our box features an assortment of edibles to meet every desire. 
+        <p>Subscription boxes are as popular as ever, but one thing has been missing from the scene… Introducing The Good Book, a cannabis subscription service that brings your high right to you. You avoid the hassle of having to even leave your apartment (because let’s be real, you’re still living in an apartment). Our books feature an assortment of edibles to meet every desire. 
+        </p>
+        <p>
+          -Jamie, CEO and Founder of The Good Book, Inc.
         </p>
 </div>
     </div>;
